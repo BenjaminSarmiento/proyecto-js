981e02237647bc1447fb392450c5cb21a5529891
Benjamin Sarmiento <benjaminsarmiento03@gmail.com>
2022-12-08 15:55:07 -0300

    entrega final

--- js/carrito.js
+++ js/carrito.js
@@ -1,4 +1,5 @@
 const carritoProceso = () => {
+    let carrito= JSON.parse(localStorage.getItem("carrito")) || [] 
     modalContainer.innerHTML=""
     modalContainer.style.display= "flex"
     const modalHeader= document.createElement("div")
@@ -26,20 +27,47 @@ const carritoProceso = () => {
         <img src="${producto.img}">
         <h2><strong>${producto.nombre}</strong></h2>
         <h4>$${producto.precio}</h4>
+        <span class= "restar"> - </span>
         <p>cantidad: ${producto.cantidad}  </p>
+        <span class= "sumar"> + </span>
         <p>total: $${producto.cantidad * producto.precio}  </p>
         `
         
         modalContainer.append(carritoContent)
-        
 
-        let eliminar = document.createElement("span")
+        let restar= carritoContent.querySelector(".restar")
+        
+        restar.addEventListener("click" ,() => {
+            if (producto.cantidad !== 1) {
+                producto.cantidad --
+            }
+            const savelocal = () => {
+                localStorage.setItem("carrito", JSON.stringify(carrito))
+               }  
+            carritoProceso()
+            savelocal()
+        })
+        let sumar= carritoContent.querySelector(".sumar")
+        
+        sumar.addEventListener("click" ,() => {
+            producto.cantidad ++
+            const savelocal = () => {
+                localStorage.setItem("carrito", JSON.stringify(carrito))
+               }  
+            carritoProceso()
+            savelocal()
+        })
+
+        let eliminar = document.createElement("span")     
         eliminar.innerText= "❌"
         eliminar.className ="botonEliminarProducto"
         carritoContent.append(eliminar)
 
         eliminar.addEventListener("click", eliminarProducto)   
+
     })
+
+   
     const total= carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0)
     const totalBuying= document.createElement("div")
     totalBuying.className= "totalContent"
@@ -52,11 +80,21 @@ const carritoProceso = () => {
 verCarrito.addEventListener("click", carritoProceso)       
 
 const eliminarProducto = () => {
+    let carrito= JSON.parse(localStorage.getItem("carrito")) || []    
     const foundId = carrito.find ((element) => element.id)
 
     carrito  = carrito.filter ((carritoId) => {
         return carritoId !== foundId
     })
+    const savelocal = () => {
+       localStorage.setItem("carrito", JSON.stringify(carrito))
+      }                                                            
+
     savelocal()
     carritoProceso()
+}
+
+const carritoCounter =() => {
+    cantidadCarrito.style.display= "block"
+    cantidadCarrito.innerText = productos.length
 }
--- js/script.js
+++ js/script.js
@@ -1,76 +1,84 @@
- let contenedorProductos = document.getElementById("contenedorProductos")
+ 
+fetch("./js/productos.json")
+.then(respuesta => respuesta.json())
+.then(productos => {
+  let contenedorProductos = document.getElementById("contenedorProductos")
     
-    const verCarrito= document.getElementById("verCarrito")
-    const modalContainer= document.getElementById("modalContainer")
-    const cantidadCarrito= document.getElementById("cantidadCarrito")
+  const verCarrito= document.getElementById("verCarrito")
+  const modalContainer= document.getElementById("modalContainer")
+  const cantidadCarrito= document.getElementById("cantidadCarrito")
 
-    let carrito= JSON.parse(localStorage.getItem("carrito")) || []    
+  let carrito= JSON.parse(localStorage.getItem("carrito")) || []    
 
-    renderizarProductos(productos)
+  renderizarProductos(productos)
 
-   function renderizarProductos(arrayProductos) {
-    contenedorProductos.innerHTML= ' '
-    for (const producto of arrayProductos) {
-        let tarjetaProductos = document.createElement("div")
-        producto.stock == 0 ? tarjetaProductos.className="tarjetaSinStock" : tarjetaProductos.className="tarjeta"
-        tarjetaProductos.innerHTML = `
-        <img class="imagenTarjeta" src=${producto.img}>
-        <h2 class="tituloTarjeta"><strong>${producto.nombre}</strong></h2>
-        <h3 class="precioTarjeta"> ${producto.caracteriticas}</h3>
-        <h4 class="precioTarjeta">$ ${producto.precio}</h4>
-        <h4> ${producto.stock} unidades disponibles</h4>
-        `
-        contenedorProductos.append(tarjetaProductos)
-        contenedorProductos.className ="tarjetas"
+ function renderizarProductos(arrayProductos) {
+  contenedorProductos.innerHTML= ' '
+  for (const producto of arrayProductos) {
+      let tarjetaProductos = document.createElement("div")
+      producto.stock == 0 ? tarjetaProductos.className="tarjetaSinStock" : tarjetaProductos.className="tarjeta"
+      tarjetaProductos.innerHTML = `
+      <img class="imagenTarjeta" src=${producto.img}>
+      <h2 class="tituloTarjeta"><strong>${producto.nombre}</strong></h2>
+      <h3 class="precioTarjeta"> ${producto.caracteriticas}</h3>
+      <span class="precioTarjeta">$ ${producto.precio}</span>
+      <h4> ${producto.stock} unidades disponibles</h4>
+      `
+      contenedorProductos.append(tarjetaProductos)
+      contenedorProductos.className ="tarjetas"
 
-        let comprar= document.createElement("button")
-        comprar.innerText="añadir al carrito"
-        comprar.className="botonAgregar"
-        tarjetaProductos.append(comprar)
+      let comprar= document.createElement("button")
+      comprar.innerText="añadir al carrito"
+      comprar.className="botonAgregar"
+      tarjetaProductos.append(comprar)
 
-        comprar.addEventListener("click", ()=>{  
-            Toastify({
-                text: "AÑADIDO CON EXITO",
-                className: "toastComprar",
-                style: {
-                    background: "linear-gradient(to right, orange, orange)",
-                  }
-              }).showToast();
-
-              const repeat= carrito.some((repeatProduct) => repeatProduct.id === producto.id)
-              if (repeat) {
-                carrito.map((prod) => {
-                if (prod.id == producto.id) {
-                    prod.cantidad ++
+      comprar.addEventListener("click", ()=>{  
+          Toastify({
+              text: "AÑADIDO CON EXITO",
+              className: "toastComprar",
+              style: {
+                  background: "linear-gradient(to right, orange, orange)",
                 }
-                })
-            } else{
-              carrito.push(
-                {
-                    id: producto.id,
-                    nombre: producto.nombre,
-                    precio: producto.precio,
-                    img: producto.img,
-                    cantidad: producto.cantidad
-                })
-            } 
-            savelocal()
-        }
-        )}
-     }
-     
-   let boton= document.getElementById("botonPrincipal")
-   boton.className= "botonPrincipal"
-   let barraDeBusqueda= document.getElementById("barraDeBusqueda")
-   barraDeBusqueda.className= "barraDeBusqueda"
-  barraDeBusqueda.addEventListener("input", fninput)
+            }).showToast();
 
-  function fninput() {
-    let productosFiltrados = productos.filter(producto => producto.nombre.includes(barraDeBusqueda.value))
-    renderizarProductos(productosFiltrados)
-  }
+            const repeat= carrito.some((repeatProduct) => repeatProduct.id === producto.id)
+            if (repeat) {
+              carrito.map((prod) => {
+              if (prod.id == producto.id) {
+                  prod.cantidad ++
+              }
+              })
+          } else{
+            carrito.push(
+              {
+                  id: producto.id,
+                  nombre: producto.nombre,
+                  precio: producto.precio,
+                  img: producto.img,
+                  cantidad: producto.cantidad
+              })
+          } 
+          savelocal()
+      }
+      )}
+   }
+   
+ let boton= document.getElementById("botonPrincipal")
+ boton.className= "botonPrincipal"
+ let barraDeBusqueda= document.getElementById("barraDeBusqueda")
+ barraDeBusqueda.className= "barraDeBusqueda"
+barraDeBusqueda.addEventListener("input", fninput)
 
-  const savelocal = () => {
-    localStorage.setItem("carrito", JSON.stringify(carrito))
-  }
+function fninput() {
+  let productosFiltrados = productos.filter(producto => producto.nombre.includes(barraDeBusqueda.value))
+  renderizarProductos(productosFiltrados)
+}
+
+const savelocal = () => {
+  localStorage.setItem("carrito", JSON.stringify(carrito))
+}
+
+ 
+})
+
   
